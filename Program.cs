bb4c738087d202895748c32910530570363c6abf
Russell Wallace <russell.wallace@gmail.com>
2024-04-30 14:30:56 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace content_type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
